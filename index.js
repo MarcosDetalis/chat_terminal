99d77ef2d4a736320b0bb4f70cb9388b015af309
MarcosDetalis <95556582+MarcosDetalis@users.noreply.github.com>
2022-01-30 13:33:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT||3000;
 const io = require("socket.io")(port);
-
+var  lisus = {}
 io.on("connect", (socket) => {
 
     console.log("Usuario conectado");
@@ -9,12 +9,37 @@ io.on("connect", (socket) => {
         console.log("\nUsuario: ", data.usuarioUnido, ", ID: ", socket.id);
         console.log("Numero de Usuarios: %d", io.of('/').server.engine.clientsCount);
         socket.usuarios= data.usuarioUnido;
+
+        lisus[data.usuarioUnido]=socket.id;//guardamos a los usarios en una lista
+      
         socket.broadcast.emit("union_recibe", data);
     });
     
     socket.on("mesaje", (data) => {
-        console.log("%s", data,"server");
-        socket.broadcast.emit("mesaje_recibe", data);
+
+        var msg = data.sms.trim();
+
+     
+      if(msg.substr(0, 5) === 'priv '){//obtenemos los datos adelante del comando priv
+        msg = msg.substr(5);
+        console.log(msg)
+        var index = msg.indexOf(' ');//buscamos el espacio para obtener los datos lo cual nos dara el indice
+        if(index !== -1){//si no tenemos errores
+            console.log(index)
+        var name = msg.substring(0, index);//obtenemos el nombre del usuario citado
+        console.log(name)
+        var msg = msg.substring(index + 1);//obtenemos el mensaje
+        console.log(msg)
+         console.log("usaioo",lisus)
+         if(name in lisus ){
+            console.log("aqui estamos")
+            io.to(lisus[name]).emit('private', {"smspriv": msg,"user":data.UsuarioSms });
+         }else{socket.emit("msg","Usuario no existe");}
+      }else{socket.emit("msg","No hay mensaje");}
+    }else{
+    console.log("%s", data,"server");
+    socket.broadcast.emit("mesaje_recibe", data);  
+    }
     });
 
     socket.on("ListaUsuario", (data) => {
